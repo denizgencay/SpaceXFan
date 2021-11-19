0d3bf2dfc4e9696e0c6c3a443f2edc42f665ffac
denizgencay <denizgencay35@gmail.com>
2021-11-19 18:11:13 +0300

    Repository and view model of Rocket added to MVVM pattern.

---
+++ app/src/main/java/com/example/denizgencayspacexfan/network/respositories/RocketRepository.kt
@@ -0,0 +1,33 @@
+package com.example.denizgencayspacexfan.network.respositories
+
+import androidx.lifecycle.MutableLiveData
+import com.example.denizgencayspacexfan.models.RocketModel
+import com.example.denizgencayspacexfan.network.ApiService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import javax.inject.Inject
+
+class RocketRepository @Inject constructor(private val apiService: ApiService) {
+
+    fun getAllRocketsFromApi(rocketDataList: MutableLiveData<List<RocketModel>>){
+        val call: Call<List<RocketModel>> = apiService.getRockets()
+        call.enqueue(object : Callback<List<RocketModel>> {
+            override fun onResponse(
+                call: Call<List<RocketModel>>,
+                response: Response<List<RocketModel>>
+            ) {
+                if (response.isSuccessful){
+                    rocketDataList.postValue(response.body())
+                    println("hoo: ${response.code()}")
+                }
+                println("hoo: ${response.code()}")
+            }
+
+            override fun onFailure(call: Call<List<RocketModel>>, t: Throwable) {
+                println("hoo: $t")
+            }
+        })
+    }
+
+}
---
+++ app/src/main/java/com/example/denizgencayspacexfan/ui/rockets/RocketsViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.denizgencayspacexfan.ui.rockets
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.denizgencayspacexfan.models.RocketModel
+import com.example.denizgencayspacexfan.network.respositories.RocketRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RocketsViewModel @Inject constructor(
+
+    private val rocketRepository: RocketRepository
+
+): ViewModel() {
+
+    var rocketsDataList: MutableLiveData<List<RocketModel>> = MutableLiveData()
+
+    fun getRocketLiveDataObserver(): MutableLiveData<List<RocketModel>> {
+        return rocketsDataList
+    }
+
+    fun loadRocketListOfData(){
+        rocketRepository.getAllRocketsFromApi(rocketsDataList)
+    }
+
+}
