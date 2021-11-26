e0dafc9b85ed11398af53530213d9e45e86e5bf8
denizgencay <denizgencay35@gmail.com>
2021-11-26 03:08:01 +0300

    Ui changes added.

--- app/src/main/java/com/example/denizgencayspacexfan/MainActivity.kt
+++ app/src/main/java/com/example/denizgencayspacexfan/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.denizgencayspacexfan
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnSystemUiVisibilityChangeListener
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.denizgencayspacexfan.ui.authentication.signin.SignInFragment
 import com.example.denizgencayspacexfan.ui.rockets.RocketsFragment
@@ -20,13 +22,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         setContentView(R.layout.activity_main)
         commitFragment(rocketsFragment)
 
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation_bar)
         bottomNavigation.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener {
-            when(it.itemId){
+            when (it.itemId) {
                 R.id.nav_bar_rockets -> commitFragment(rocketsFragment)
                 R.id.nav_bar_favorites -> commitFragment(signInFragment)
                 R.id.nav_bar_upcoming -> commitFragment(upcomingFragment)
@@ -37,7 +43,7 @@ class MainActivity : AppCompatActivity() {
     //Setting bottom navigation bar selections
     private fun commitFragment(fragment: Fragment){
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container,fragment)
+        transaction.replace(R.id.fragment_container, fragment)
         transaction.commit()
     }
 
